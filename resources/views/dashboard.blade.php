a03f43176757ae2c969d3c1368a8d40db860e0cb
Maxim Fateyev <maxim.f@rocketfirm.com>
2024-07-09 15:15:19 +0500

    new address

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,5 +1,6 @@
 @if(isset($config->address)) @section( 'chinaaddress', $config->address ) @endif
 @if(isset($config->tag)) @section( 'tag', $config->tag ) @endif
+@if(isset($config->title_text)) @section( 'title_text', $config->title_text ) @endif
 @if(isset($config->address_two)) @section( 'address_two', $config->address_two ) @endif
 <x-app-layout>
         <div class="py-6">
